6f66aae10100ccb8680ac4afe5b3a43ed25e38b0
rjanjanam <ratna.janjanam@rms.com>
2019-09-29 12:47:19 -0700

    testing

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,16 @@
+public class Constants {
+
+    //List of System Variables
+    public static final String URL = "https://login.salesforce.com/";
+
+
+    //List of Test Data
+    public static final String UserName = "testuser_1";
+    public static final String Password = "Test@123";
+
+    public static String stageStatus = "prospecting";
+    public static String accountName = "TestAutomation";
+    public static String amount = "100";
+
+
+}
